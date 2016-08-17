0f6f1a831be87be802dcb9e8995231d552a8609d
Berler666 <destroyed76@hotmail.com>
2016-08-17 21:15:34 +1000

    fixed bug with shooting capsules in rts mode. fixed crosshair being active in rts mode

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour {
     public GameObject RTSCamera;
     public GameObject FPSCamera;
 
-    public Transform player;
+    public GameObject player;
 
     public GameObject prefab;
+
+    public GameObject fpsCanvas;
 
     public float thrust;
 
@@ -23,6 +25,10 @@ public class PlayerController : MonoBehaviour {
         RTSCamera.SetActive(true);
         FPSCamera.SetActive(false);
         RtsCamera = true;
+        Cursor.visible = true;
+        fpsCanvas.SetActive(false);
+        player.SetActive(false);
+        
     }
 	
 	// Update is called once per frame
@@ -40,24 +46,31 @@ public class PlayerController : MonoBehaviour {
                 RTSCamera.SetActive(false);
                 FPSCamera.SetActive(true);
                 RtsCamera = false;
+                Cursor.visible = false;
+                player.SetActive(true);
+                fpsCanvas.SetActive(true);
             }
             else if(RtsCamera == false)
             {
                 RTSCamera.SetActive(true);
                 FPSCamera.SetActive(false);
                 RtsCamera = true;
+                Cursor.visible = true;
+                player.SetActive(false);
+                fpsCanvas.SetActive(false);
             }
         }
 
-        if (FPSCamera == true)
+        if (RTSCamera == false)
         {
 
             if (Input.GetKeyDown("e"))
             {
+                Transform playerPos = player.GetComponent<Transform>();
                 GameObject create;
                 create = Instantiate(prefab, player.transform.position, player.transform.rotation) as GameObject;
 
-                create.GetComponent<Rigidbody>().AddForce(player.forward * thrust);
+                create.GetComponent<Rigidbody>().AddForce(playerPos.forward * thrust);
             }
         }
 
