4bcae8113cb2e8c75ec409d2ca21dee1790f7108
Berler666 <destroyed76@hotmail.com>
2016-11-07 23:32:41 +1100

    added new font, added slection triangle to main menu buttons, added panels for each button in menu, clicking a button will show its panel

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -4,11 +4,56 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
+    public GameObject singlePlayerMenu;
+    public GameObject multiplayerMenu;
+    public GameObject loadGameMenu;
+    public GameObject mainMenuOptionsMenu;
+
+    
+    void Start()
+    {
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        loadGameMenu.SetActive(false);
+        mainMenuOptionsMenu.SetActive(false);
+    }
+
     public void SinglePlayer()
+    {
+        multiplayerMenu.SetActive(false);
+        loadGameMenu.SetActive(false);
+        mainMenuOptionsMenu.SetActive(false);
+        singlePlayerMenu.SetActive(true);
+    }
+
+    public void Multiplayer()
+    {
+        singlePlayerMenu.SetActive(false);
+        loadGameMenu.SetActive(false);
+        mainMenuOptionsMenu.SetActive(false);
+        multiplayerMenu.SetActive(true);
+    }
+
+    public void LoadGame()
+    {
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        mainMenuOptionsMenu.SetActive(false);
+        loadGameMenu.SetActive(true);
+    }
+
+    public void Options()
+    {
+        singlePlayerMenu.SetActive(false);
+        multiplayerMenu.SetActive(false);
+        loadGameMenu.SetActive(false);
+        mainMenuOptionsMenu.SetActive(true);
+    }
+
+    public void Skirmish()
     {
         SceneManager.LoadScene("Valley River");
     }
-
 
     public void QuitGame()
     {
