6061ce555d0eaee3e6b7af4b1c17878ffcd32d16
Berler666 <destroyed76@hotmail.com>
2016-11-15 20:33:17 +1100

    player can now deposit wood at a stockpile

--- Assets/woodStockpile.cs
+++ Assets/woodStockpile.cs
@@ -1,15 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class woodStockpile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+   public GameObject stockPileUI;
+    bool uiActive = false;
+
+    Transform player;
+
+    public Text text;
+
+    PlayerInventory playerinv;
+
+    void Start () {
+        stockPileUI.SetActive(false);
+        stockPileUI.GetComponent<Transform>();
+        playerinv = GameObject.Find("Player Controller").GetComponent<PlayerInventory>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+
+        if (uiActive == true)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            Vector3 targetPostition = new Vector3(player.position.x, stockPileUI.transform.position.y, player.position.z);
+            stockPileUI.transform.LookAt(targetPostition);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Deposit();
+
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            stockPileUI.SetActive(true);
+            uiActive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            stockPileUI.SetActive(false);
+            
+        }
+    }
+
+    void Deposit()
+    {
+        playerinv.pWood += PlayerResources.wood;
+        text.text = "Plus " + playerinv.pWood + " Wood";
+        playerinv.pWood -= playerinv.pWood;
+        StartCoroutine(HideText());
+    }
+
+    IEnumerator HideText()
+    {
+        yield return new WaitForSeconds(4);
+        text.text = "  ";
+    }
 }
