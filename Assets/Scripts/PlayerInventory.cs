470821c7d100975b76202f40ab9f13d40344e760
Berler666 <destroyed76@hotmail.com>
2016-11-15 21:05:47 +1100

    added visual side to player inventory

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour {
 
@@ -16,13 +17,52 @@ public class PlayerInventory : MonoBehaviour {
     public bool canCollectGold = false;
 
 
+    public GameObject playerInv;
+
+    bool invIsOpen = false;
+
+    public Text foodTxt;
+    public Text woodTxt;
+    public Text stoneTxt;
+    public Text GoldTxt;
+
     // Use this for initialization
     void Start () {
-	
-	}
+
+        foodTxt.GetComponent<Text>();
+        woodTxt.GetComponent<Text>();
+        stoneTxt.GetComponent<Text>();
+        GoldTxt.GetComponent<Text>();
+
+        playerInv.SetActive(false);
+
+    }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if(invIsOpen == false)
+            {
+                playerInv.SetActive(true);
+                invIsOpen = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                foodTxt.text = "Food " + pFood;
+                woodTxt.text = "Wood " + pWood;
+                stoneTxt.text = "Stone " + pStone;
+                GoldTxt.text = "Gold " + pGold;
+            }
+            else
+            {
+                playerInv.SetActive(false);
+                invIsOpen = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+                
+        }
+
         if (pFood >= carryLimit)
         {
             canCollectFood = false;
@@ -47,6 +87,12 @@ public class PlayerInventory : MonoBehaviour {
 
         }
 
+    }
+
+    public void CloseInv()
+    {
+        playerInv.SetActive(false);
+        invIsOpen = false;
     }
 
 }
